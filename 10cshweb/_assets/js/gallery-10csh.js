2e943d07110bf623bdaf9d8d2a2641a2bb2967bd
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2014-03-18 17:17:37 -0600

    10th CSH Gallery mods:
    Deleted unit-gallery folder
    Added galleries folder
    Unit pages have their galleries added
    Updated gallery-10csh.js file to include new function calls for the new gallery pages

--- 10cshweb/_assets/js/gallery-10csh.js
+++ 10cshweb/_assets/js/gallery-10csh.js
@@ -1,16 +1,73 @@
 $(function () {
 	
-	$('#ball').jflickrfeed({
+	$('#gallery-events').jflickrfeed({
+		qstrings: {
+			id: '120064813@N04',
+			photoset_id: '72157642209082125'
+		},
+		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a>'
+	});
+	
+	$('#gallery-ball').jflickrfeed({
 		qstrings: {
 			id: '120064813@N04',
 			photoset_id: '72157642213569784'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a>'
 	});
-	$('#alpha-gallery').jflickrfeed({
+	
+	$('#gallery-ftx').jflickrfeed({
+		qstrings: {
+			id: '120064813@N04',
+			photoset_id: '72157642212452043'
+		},
+		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a>'
+	});
+	
+	$('#gallery-alpha').jflickrfeed({
 		qstrings: {
 			id: '120064813@N04',
 			photoset_id: '72157642212734173'
+		},
+		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	});
+	
+	$('#gallery-438').jflickrfeed({
+		qstrings: {
+			id: '120064813@N04',
+			photoset_id: '72157642212734123'
+		},
+		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	});
+	
+	$('#gallery-223').jflickrfeed({
+		qstrings: {
+			id: '120064813@N04',
+			photoset_id: '72157642212734103'
+		},
+		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	});
+	
+	$('#gallery-2fst').jflickrfeed({
+		qstrings: {
+			id: '120064813@N04',
+			photoset_id: '72157642212734043'
+		},
+		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	});
+	
+	$('#gallery-hhd').jflickrfeed({
+		qstrings: {
+			id: '120064813@N04',
+			photoset_id: '72157642212452063'
+		},
+		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	});
+	
+	$('#gallery-bravo').jflickrfeed({
+		qstrings: {
+			id: '120064813@N04',
+			photoset_id: '72157642212452013'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
 	});
