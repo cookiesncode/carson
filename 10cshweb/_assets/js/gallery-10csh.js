183a921dcfbb9d788e05af00adb429ed20f7bc1e
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2014-03-20 09:38:27 -0600

    Modified gallery-10csh.js file
    Pre loading text "Loading images from Flickr..." before image load and removing it when complete

--- 10cshweb/_assets/js/gallery-10csh.js
+++ 10cshweb/_assets/js/gallery-10csh.js
@@ -1,4 +1,6 @@
 $(function () {
+	var $loadtext = $('<p id="load-text">Loading images from Flickr...</p>');
+	$('.gallery').append($loadtext);
 	
 	$('#gallery-events').jflickrfeed({
 		qstrings: {
@@ -6,6 +8,8 @@ $(function () {
 			photoset_id: '72157642209082125'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a>'
+	}, function (data) {
+		$('#load-text').remove();
 	});
 	
 	$('#gallery-ball').jflickrfeed({
@@ -14,6 +18,8 @@ $(function () {
 			photoset_id: '72157642213569784'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a>'
+	}, function (data) {
+		$('#load-text').remove();
 	});
 	
 	$('#gallery-ftx').jflickrfeed({
@@ -22,6 +28,8 @@ $(function () {
 			photoset_id: '72157642212452043'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a>'
+	}, function (data) {
+		$('#load-text').remove();
 	});
 	
 	$('#gallery-alpha').jflickrfeed({
@@ -30,6 +38,8 @@ $(function () {
 			photoset_id: '72157642212734173'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	}, function (data) {
+		$('#load-text').remove();
 	});
 	
 	$('#gallery-438').jflickrfeed({
@@ -38,6 +48,8 @@ $(function () {
 			photoset_id: '72157642212734123'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	}, function (data) {
+		$('#load-text').remove();
 	});
 	
 	$('#gallery-223').jflickrfeed({
@@ -46,6 +58,8 @@ $(function () {
 			photoset_id: '72157642212734103'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	}, function (data) {
+		$('#load-text').remove();
 	});
 	
 	$('#gallery-2fst').jflickrfeed({
@@ -54,6 +68,8 @@ $(function () {
 			photoset_id: '72157642212734043'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	}, function (data) {
+		$('#load-text').remove();
 	});
 	
 	$('#gallery-hhd').jflickrfeed({
@@ -62,6 +78,8 @@ $(function () {
 			photoset_id: '72157642212452063'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	}, function (data) {
+		$('#load-text').remove();
 	});
 	
 	$('#gallery-bravo').jflickrfeed({
@@ -70,6 +88,8 @@ $(function () {
 			photoset_id: '72157642212452013'
 		},
 		itemTemplate: '<a class="th radius" data-gallery href="{{image_b}}"><img src="{{image_q}}" alt="{{title}}" /></a></li>'
+	}, function (data) {
+		$('#load-text').remove();
 	});
 	
 }); // end document.ready()
