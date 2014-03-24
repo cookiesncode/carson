af7919551eed4b16ef89722fc43b997806c2bfe9
Miguel Gomez <gomail.gomez@gmail.com>
2014-03-23 20:04:46 -0600

    Modified jflickrfeed-api-custom:
    Re-wrote plug-in for readability

--- fortcarson-2014/includes/js/vendor/jflickrfeed-api-custom.js
+++ fortcarson-2014/includes/js/vendor/jflickrfeed-api-custom.js
@@ -2,7 +2,9 @@
 (function($){
 	
 	$.fn.jflickrfeed = function (settings, callback) {
+
 		settings = $.extend(true, {
+
 			flickrbase: 'http://api.flickr.com/services/rest/',
 			feedapi: '',
 			limit: 50,
@@ -16,24 +18,78 @@
 			cleanDescription: true,
 			useTemplate: true,
 			itemTemplate: '',
-			itemCallback: function() {}
+			itemCallback: function () {}
 		}, settings);	
 		
 		var url = settings.flickrbase +	settings.feedapi +'?';
 		var is_first = true;
 		
 		for (var key in settings.qstrings) {
+
 			if (!is_first) {
+
 				url += '&';
 				url += key + '=' + settings.qstrings[key];
 				is_first = false;
 			}
 		}
 		return $(this).each(function () {
+
 			var $container = $(this);
 			var container = this;
 			
-			// start here miguel
+			$.getJSON('url', function (data) {
+
+				$.each(data.photoset.photo, function (i, item) {
+
+					if (i < settings.limit) {
+
+						if (settings.cleanDescription) {
+
+							var regex = /<p>(.*?)<\/p>/g;
+							var input = item.description;
+
+							if (regex.test(input)) {
+
+								item.description = input.match(regex)[2];
+
+								if (item.description !== undefined) {
+
+									item.description = item.description.replace('<p>', '').replace('</p>', '');
+								}
+							}
+						}
+
+						item['image'] = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '.jpg';
+						item['image_s'] = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_s.jpg';
+						item['image_q'] = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_q.jpg';
+						item['image_t'] = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_t.jpg';
+						item['image_m'] = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_m.jpg';
+						item['image_b'] = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_b.jpg';
+
+						//Use template
+						if (settings.useTemplate) {
+
+							var template = settings.itemTemplate;
+							for (var key in item) {
+
+								var rgx = new RegExp('{{' + key + '}}', 'g');
+								template = template.replace(rgx, item[key]);
+							}
+							$container.append(template);
+						}
+
+						//itemCallback
+						settings.itemCallback.call(container, item);
+						
+					}
+				}); // end .each()
+				
+				if ($.isFunction(callback)) {
+
+					callback.call(container, data);
+				};
+			}); // end getJSON
 		});// end .each()
 		
 	};// end $.fn.jflickrfeed
