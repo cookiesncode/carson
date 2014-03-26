918e3e80d10a99beb6669f9d20635560033a65a2
unknown <miguel.gomez5.ctr@CARSNECXNBX0169.nanw.ds.army.mil>
2014-03-26 13:14:55 -0600

    Orbit update:
    Moved orbit-arrows functionality into the callback function of the jflickrfeed function call.
    Removed unnecessary option being passed to the foundation orbit function call.

--- fortcarson-2014/includes/js/app.js
+++ fortcarson-2014/includes/js/app.js
@@ -1,19 +1,4 @@
 $(document).ready( function () {
-
-// ORBIT ///////////////////////////// 
-
-// Control Orbit carousel control arrows visibility	
-	var $orbit_arrows = $( '.orbit-prev, .orbit-next' );
-	$orbit_arrows.addClass( 'hide-for-medium-up' );
-	
-	if ( $orbit_arrows.is( ':hidden' ) ) {
-		$( '.orbit-container' ).hover( function () {
-				$orbit_arrows.removeClass( 'hide-for-medium-up' );
-			}, function () {
-					$orbit_arrows.addClass( 'hide-for-medium-up' );
-				}
-			);
-	}
 	
 // ROTATING UNIT CREST ///////////////////////////// 
 	
