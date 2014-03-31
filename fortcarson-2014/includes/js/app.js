ec3906053728d73837a4c098d0bcf0aa670a036e
Miguel Gomez <miguel.gomez16.ctr@mail.mil>
2014-03-31 15:44:56 -0600

    Master template update:
    -Template was changed to reflect how scripts are loaded.
    -Modernizr is loaded after the CSS
    -Most of the custom JS is now being controlled throught app.js which imports most dependencies such as jquery. This is being handled by Prepros.
    -The blueimp gallery plugin is still being loaded on a per page basis. Along with the custom flickr setup for 10thCSH

--- fortcarson-2014/includes/js/app.js
+++ fortcarson-2014/includes/js/app.js
@@ -1,18 +1,25 @@
-//@prepros-prepend plugins/jflickrfeed-api-custom.js
 //@prepros-prepend plugins/slideshow.js
 //@prepros-prepend plugins/jquery.fittext.js
 //@prepros-prepend plugins/jquery.elastislide.js
 //@prepros-prepend plugins/jquerypp.custom.js
+//@prepros-prepend plugins/jflickrfeed-api-custom.js
 //@prepros-prepend plugins/jquery.url.js
 //@prepros-prepend min/foundation.min.js
+//@prepros-prepend min/jquery.min.js
 
 // Immediately Invoked Function Expression (IIFE)
 // This pattern keeps the variables from being global.
 (function($) {
 $(document).ready(function () {
+	
 	$(document).foundation();
+	
+// MISCELLANEOUS ///////////////////////////// 
+	$("#crests > img").css("display", "inline");
+	$("#fittext").fitText(1.5, { minFontSize: '18px', maxFontSize: '30px'});
+	
 // DISCLAIMER MODAL ///////////////////////////// 
-	if ($.cookie('disclaimer') !== 'checked' ) {
+	if ($.cookie('disclaimer') && $.cookie('disclaimer') !== 'checked' ) {
 		$('#disclaimer-link').trigger('click');
 		$.cookie('disclaimer', 'checked', { expires: 1 });
 	}
@@ -132,13 +139,9 @@ $(document).ready(function () {
 	});
 
 // EXTERNAL LINK WARNING ///////////////////////////// 
-	// $("[rel='external']").click(function() {
-	// 	alert ("NOTE: You are now leaving the Fort Carson Website. The appearance of hyperlinks does not constitute endorsement by the Department of Defense (DoD), the U.S. Army, or Fort Carson of the linked websites or the information, products or services contained therein. For other than authorized activities such as military exchanges and Morale, Welfare and Recreation sites, the Department of Defense, the U.S. Army and Fort Carson does not exercise any editorial control over the information you may find at these locations. Such links are provided consistent with the stated purpose of this DoD website. We consider an external link to be a hyperlink to non-DoD web resources.");
-	// });
-// MISCELLANEOUS ///////////////////////////// 
-	
-	$("#crests > img").css("display", "inline");
-	$("#fittext").fitText(1.5, { minFontSize: '18px', maxFontSize: '30px'});
+	 $("[rel='external']").click(function() {
+	 	alert ("NOTE: You are now leaving the Fort Carson Website. The appearance of hyperlinks does not constitute endorsement by the Department of Defense (DoD), the U.S. Army, or Fort Carson of the linked websites or the information, products or services contained therein. For other than authorized activities such as military exchanges and Morale, Welfare and Recreation sites, the Department of Defense, the U.S. Army and Fort Carson does not exercise any editorial control over the information you may find at these locations. Such links are provided consistent with the stated purpose of this DoD website. We consider an external link to be a hyperlink to non-DoD web resources.");
+	 });
 		
 		
 });// end document.ready function
